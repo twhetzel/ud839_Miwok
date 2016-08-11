d385c1f1f5081a687463acf0dd02479b41473396
Trish Whetzel <plwhetzel@gmail.com>
2016-08-10 17:24:23 -0700

    use ArrayList for words

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
     private final static String TAG = "NumbersActivity";
 
@@ -12,20 +14,22 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        for(int i=0; i < words.length; i++) {
-            Log.d(TAG, "Word at index "+i+": "+words[i]);
+        int i = 0;
+        for (String word : words) {
+            Log.d(TAG, "Word at index "+ i + ": " + word);
+            i++;
         }
     }
 }
