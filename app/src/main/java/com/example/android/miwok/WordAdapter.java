ac002d15508bf9e1dfb94356602db24f9b0a6c61
Trish Whetzel <plwhetzel@gmail.com>
2016-08-13 01:02:31 -0700

    add custom adapter for Word

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by whetzel on 8/12/16.
+ */
+public class WordAdapter extends ArrayAdapter<Word> {
+    private static final String LOG_TAG = WordAdapter.class.getSimpleName();
+
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+//        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+//        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+//        // Get the image resource ID from the current AndroidFlavor object and
+//        // set the image to iconView
+//        iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+
+}
